53cad6f03ed438e7b10b56762fa8d9472b71f0a1
pheianox <pheianox@gmail.com>
2022-11-15 13:50:41 +0400

    init

--- src/lib/core.ts
+++ src/lib/core.ts
@@ -1,23 +1,26 @@
 import { createSignal, createMemo } from "solid-js"
 
+const TEMPLATE_REGEX = /{{(.*?)}}/g
+const PATH_DELIMINATOR = "."
+
 export const createI18nContext = <T>(dictinary_: T, language_: keyof T) => {
   const [language, setLanguage] = createSignal(language_)
   const [dictinary, setDictinary] = createSignal(dictinary_)
-  const languages = () => Object.keys(dictinary() as object) as (keyof T)[]
   const translations = createMemo(() => dictinary()[language()])
+  const languages = () => Object.keys(dictinary() as object) as (keyof T)[]
 
   function lookup(object: T[keyof T], path: string[], defaultValue: any) {
-    // @ts-ignore
     const value = path.reduce((obj, key) => obj ? obj[key] : undefined, object)
     return value === undefined ? defaultValue : value
   }
 
   function substitute(translation: string, params: Params) {
-    return translation.replace(/{{(.*?)}}/g, (_, param) => params[param])
+    return translation.replace(TEMPLATE_REGEX, (_, param) => params[param])
   }
 
   function translate(path: string, params?: Params) {
-    const value = lookup(translations(), path.trim().split("."), "")
+    const keyList = path.trim().split(PATH_DELIMINATOR)
+    const value = lookup(translations(), keyList, "")
     switch (typeof value) {
       case "function": return value(params)
       case "string": return params ? substitute(value, params) : value
@@ -25,5 +28,5 @@ export const createI18nContext = <T>(dictinary_: T, language_: keyof T) => {
     }
   }
 
-  return { translate, language, languages, setLanguage, dictinary, setDictinary }
+  return { translate, languages, language, setLanguage, dictinary, setDictinary }
 }
